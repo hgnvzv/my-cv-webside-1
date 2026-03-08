6639af84c2e0aa8214d67b991789156cdf552a6e
mohammed_qasem <86741753+hgnvzv@users.noreply.github.com>
2026-03-08 10:03:08 +0300

    Add contact information section with email

--- pages/index.vue
+++ pages/index.vue
@@ -805,6 +805,24 @@
                 </svg>
                 07707752085
               </p>
+              <div class="space-y-2 text-slate-400">
+                <p class="flex items-center gap-2">
+                  <svg
+                    class="w-5 h-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                    />
+                  </svg>
+                  atfsucy.com@gmail.com
+                </p>
+              </div>
               <p class="flex items-center gap-2">
                 <svg
                   class="w-5 h-5"
